2ae81052ab246f28d49351f394e67e53b7880e83
LuizHenriqueT1 <luiz.hesoaress@gmail.com>
2024-01-04 12:31:52 -0300

    FEAT: Add informações de success e error ao enviar message.

--- str-producer/src/main/java/com/rick/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/rick/strproducer/services/StringProducerService.java
@@ -4,13 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class StringProducerService {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if (success != null) {
+                        System.out.println("Send message with success: " + message);
+                        System.out.println("Partition: " + success.getRecordMetadata().partition());
+                        System.out.println("OffSet: " + success.getRecordMetadata().offset());
+                        System.out.println("Topic: " + success.getProducerRecord().topic());
+                    }
+                },
+                error -> System.out.println("Send message with error: " + message)
+        );
     }
 }
